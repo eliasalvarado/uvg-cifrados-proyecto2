15d902bff56a575c75ff04fbe8162af30768f023
Diego Andrés Morales Aquino <67699784+Aq202@users.noreply.github.com>
2025-05-30 21:05:50 -0600

    fix: remover sockets de useChatState

--- frontend/src/hooks/useChatState.js
+++ frontend/src/hooks/useChatState.js
@@ -1,66 +1,10 @@
-import { useContext, useEffect, useRef } from 'react';
-import getTokenPayload from '../helpers/getTokenPayload';
-import useToken from './useToken';
-import { io } from 'socket.io-client';
+import { useContext } from 'react';
 import ChatContext from '../context/ChatContext';
 
 function useChatState() {
 
 const {messages, setMessages} = useContext(ChatContext);
-  
-  const token = useToken();
-  const socketRef = useRef(null); // referencia para mantener el socket entre renders
 
-  useEffect(() => {
-    if (!token) {
-      if (socketRef.current) {
-        socketRef.current.disconnect();
-      }
-      return;
-    }
-
-    const userData = getTokenPayload(token);
-    if (!userData) {
-      console.error('Invalid token');
-      return;
-    }
-
-    // crear socket con autenticación
-    socketRef.current = io('http://localhost:3000', {
-      auth: { token },
-    });
-
-    const socket = socketRef.current;
-
-    console.log('Connecting to socket with user ID:', userData.id);
-
-    socket.on('connect', () => {
-      console.log('Socket connected:', socket.id);
-    });
-
-    socket.on('chat_message', (data) => {
-        console.log('Received chat message:', data);
-      if (data.to === userData.id) {
-        console.log('Mensaje recibido:', data.message);
-        // Aquí podrías actualizar tu estado con el nuevo mensaje
-      }
-    });
-
-    socket.on('disconnect', () => {
-      console.log('Socket disconnected');
-    });
-
-    socket.on('connect_error', (err) => {
-        console.error('Error de conexión socket:', err.message);
-    });
-
-    return () => {
-      socket.off('connect');
-      socket.off('disconnect');
-      socket.off('chat_message');
-      socket.disconnect(); // desconectar socket al desmontar
-    };
-  }, [token]); // [ idUsuario: [{id, message}, {id, message}, ...], ...]
 
     const addMessage = (message) => {
         const originUserId = message.from;
