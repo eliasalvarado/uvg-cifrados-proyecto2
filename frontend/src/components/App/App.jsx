61b247174c096a57d03583758a3da3f259955afa
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2025-10-25 01:00:00 -0600

    🐛 Fix: App - Component

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,8 +1,5 @@
-import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import IndexPage from '../../pages/IndexPage/IndexPage';
-import RegisterPage from '../../pages/RegisterPage/RegisterPage';
-import OAuthSuccessPage from '../../pages/OAuthSuccessPage/OAuthSuccessPage';
 import { SessionProvider } from '../../context/SessionContext';
 import { ChatProvider } from '../../context/ChatContext';
 import { SocketProvider } from '../../context/SocketContext';
