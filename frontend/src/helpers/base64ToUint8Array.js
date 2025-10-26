3f79339e8eeee80ba6720d826356e83614f8dced
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2025-10-25 19:28:20 -0600

    🐛 Fix: base64ToUint8Array

--- frontend/src/helpers/base64ToUint8Array.js
+++ frontend/src/helpers/base64ToUint8Array.js
@@ -1,2 +1,2 @@
-const base64ToUint8Array = (base64) => Uint8Array.from(atob(base64), c => c.charCodeAt(0));
+const base64ToUint8Array = (base64) => Uint8Array.from(atob(base64), c => c.codePointAt(0));
 export default base64ToUint8Array;
